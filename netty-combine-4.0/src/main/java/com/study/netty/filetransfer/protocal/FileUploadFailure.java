1c4d7a857abde8607b79592bb806e197718edf79
wangqiang <874123073@qq.com>
2018-09-19 18:29:45 +0800

    filetransfer

---
+++ netty-combine-4.0/src/main/java/com/study/netty/filetransfer/protocal/FileUploadFailure.java
@@ -0,0 +1,7 @@
+package com.study.netty.filetransfer.protocal;
+
+/**
+ * Created by Hunter on 2017/04/14.
+ */
+public class FileUploadFailure {
+}
---
+++ netty-combine-4.0/src/main/java/com/study/netty/filetransfer/protocal/MessageWithHeader.java
@@ -0,0 +1,7 @@
+package com.study.netty.filetransfer.protocal;
+
+/**
+ * Created by Hunter on 2017/04/18.
+ */
+public class MessageWithHeader {
+}
